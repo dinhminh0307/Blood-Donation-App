41cdf9be74064c9a1f4c95da890b66a2ac510d5e
dinhminh0307 <dinhminh181003@gmail.com>
2024-12-12 16:39:46 +0700

    feat: add google map not yet add donation site marker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     // Add the Google services Gradle plugin
     id("com.google.gms.google-services")
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
@@ -31,6 +32,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,6 +43,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/blooddonate/activities/HomePageActivity.java
+++ app/src/main/java/com/example/blooddonate/activities/HomePageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +16,14 @@ import com.example.blooddonate.R;
 
 public class HomePageActivity extends AppCompatActivity {
     ImageView findSiteButton;
+    TextView siteMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
         onFindSiteBtnClicked();
+        onSiteMapClicked();
     }
 
     private void onFindSiteBtnClicked() {
@@ -29,6 +32,17 @@ public class HomePageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomePageActivity.this, FindSiteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onSiteMapClicked() {
+        siteMap = findViewById(R.id.nearby_donation_sites);
+        siteMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePageActivity.this, MapsActivity.class);
                 startActivity(intent);
             }
         });
